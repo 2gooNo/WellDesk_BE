d57faa26ee6f3c87450176f3806bdf967f8672a5
Ideree0316 <ideree03161@gmail.com>
2026-07-08 12:53:50 +0800

    user_model

---
+++ src/models/user_models.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    medicine_schedule:[
+        {
+            medicine_name: {
+                type: String,
+                required: true},
+            info: [
+                {
+                    day: {type: Number,required: true},
+                    when: [
+                        {
+                            time: {type: Number, required: true},
+                            quantity: {type: Number, required: true}
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    month_water_history:[
+        {
+            date: {type: Date, required: true},
+            value: {type: Number, required: true}
+        }
+    ],
+    month_butt_history:[
+        {
+            date: {type: Date, required: true},
+            value: {type: Number, required: true}
+        }
+    ],
+    back_error:[
+        {
+            when: {type: Date, required: true},
+            off_angle: {type: Number, required: true},
+        }
+    ],
+    month_back_history:[
+        {
+            date: {type: Date, required: true},
+            value: {type: Number, required: true}
+        }
+    ],
+    back_mean:[
+        {
+            date: {type: Date, required: true},
+            value: {type: Number, required: true}
+        }
+    ],
+    butt_mean:[
+        {
+            col: {type: Number, required: true},
+            row: {type: Number, required: true},
+            value: {type: Number, required: true}
+        }
+    ],
+    displayName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    daily_water_goal: {type: Number, required: true},
+
+})
+export const UserModel = mongoose.model("user", userSchema)
