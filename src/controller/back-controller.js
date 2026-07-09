1ff28810102647238e2f0d7a19451c66eb0104ae
Ideree0316 <ideree03161@gmail.com>
2026-07-09 14:17:25 +0800

    peak version of back-controller

--- src/controller/back-controller.js
+++ src/controller/back-controller.js
@@ -1,41 +1,80 @@
-import {UserModel} from '../models/user_models.js';
+import { UserModel } from "../models/user_models.js";
 
 export const BackData = async (req, res) => {
-
     try {
+
         const { userID, angle } = req.body;
+
         const user = await UserModel.findById(userID);
 
-        if(!user) {
-            return res.status(404).json({ message: "User not found"});
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
         }
-        user.back_mean =  (angle + user.back_mean) / 2;
-        await user.save();
-        res.status(200).json({ message: "Back data updated successfully", back_mean: user.back_mean });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
 
-export const DailyPush  = async (req, res) => {
+        user.back_mean += angle;
+        user.back_count++;
+
+        await user.save();
+
+        res.status(200).json({
+            message: "Back data updated successfully"
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
+
+    }
+};
+
+export const DailyPush = async (req, res) => {
 
     try {
-        const userID = req.body;
+
+        const { userID } = req.body;
+
         const user = await UserModel.findById(userID);
 
-        if(!user) {
-            return res.status(404).json({ message: "User not found"});
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
         }
-        user.month_back_history.push({ 
+
+        let average = 0;
+
+        if (user.back_count > 0) {
+            average = user.back_mean / user.back_count;
+        }
+
+        user.month_back_history.push({
             date: new Date(),
-            value: user.back_mean 
+            value: average
         });
+
         user.back_mean = 0;
+        user.back_count = 0;
+
         await user.save();
-        res.status(200).json({ message: "Daily back data pushed successfully", month_back_history: user.month_back_history });
+
+        res.status(200).json({
+            message: "Daily back data pushed successfully",
+            today_average: average
+        });
+
     } catch (error) {
+
         console.error(error);
-        res.status(500).json({ message: "Internal server error" });
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
+
     }
-}
+};
